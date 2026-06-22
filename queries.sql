f413e91eec899217205fba11900245297e4ae53f
Biniyam Data Scientist <yottor12@gmail.com>
2026-06-22 13:40:16 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,52 +2,65 @@
 -- SECTION A: FUNDAMENTALS
 -- ==================================================
 -- a
-select first_name, 
+-----------------------------------------------------
+SELECT first_name, 
 		last_name, 
         email 
-from customers
-where city = 'Lagos'
-order by last_name, first_name;
-
+FROM customers
+WHERE city = 'Lagos'
+ORDER BY last_name, first_name;
+-----------------------------------------------------
 -- b
+-----------------------------------------------------
 SELECT DISTINCT shipping_city
 FROM orders
 ORDER BY shipping_city;
+-----------------------------------------------------
 -- c
-select product_name, 
+-----------------------------------------------------
+SELECT product_name, 
 		category_id, 
         unit_price
- from products
- order by unit_price desc
- limit 10;
+ FROM products
+ ORDER BY unit_price DESC
+ LIMIT 10;
+-----------------------------------------------------
  -- d
- select concat(first_name, ' ', last_name) as full_name, 
+-----------------------------------------------------
+ SELECT concat(first_name, ' ', last_name) as full_name, 
 		role, 
         hire_date, 
         salary 
- from employees
- where hire_date >= '2021-01-01'
- order by hire_date;
+ FROM employees
+ WHERE hire_date >= '2021-01-01'
+ ORDER BY hire_date;
+-----------------------------------------------------
  -- e
- select order_id,
+-----------------------------------------------------
+ SELECT order_id,
 		order_date, 
         status, 
         shipping_city
- from orders
- where month(order_date) = 12
- order by order_date desc;
+ FROM ordersWHERE EXTRACT(MONTH FROM order_date) = 12
+ORDER BY order_date DESC;
  
  -- ==================================================
  -- SECTION B: AGGREGATE FUNCTIONS
  -- ==================================================
  -- a
- select status,
-		count(*) as count, 
-        round(count(*) / 500 * 100, 2) as pct_of_total 
- from orders
- group by status
- order by count desc;
+-----------------------------------------------------
+ SELECT status,
+		ROUND(
+    COUNT(*) * 100.0 /
+    (SELECT COUNT(*) FROM orders),
+    2
+) AS pct_of_total
+ FROM orders
+ GROUP BY status
+ ORDER BY pct_of_total DESC;
+-----------------------------------------------------
  -- b
+-----------------------------------------------------
  select category_name, 
 		min(unit_price) as min_price, max(unit_price) as max_price, 
         round(avg(unit_price), 2) as average_price
@@ -56,16 +69,20 @@ select product_name,
  using(category_id)
  group by category_name
  order by average_price desc;
+-----------------------------------------------------
  -- c
+-----------------------------------------------------
  select round(sum(revenue), 2) as total_revenue, 
 		round(min(revenue),2) as minimun_revenue, 
         round(max(revenue),2) as maximum_revenue, 
         round(avg(revenue),2) as average_revenue from (
  select 
-	quantity * (unit_price -  discount) as revenue
+	quantity * unit_price *(1 -  discount / 100.0) as revenue
  from order_items
  ) as revenue_table;
+-----------------------------------------------------
  -- d
+-----------------------------------------------------
  select count(distinct customer_id) as total_customers_with_orders,
 		round(avg(order_count), 2) as avg_orders_per_customers
  from (
@@ -77,13 +94,16 @@ select product_name,
   -- =========================================================
  -- SECTION C: GROUPING
  -- a 
+-----------------------------------------------------
  select year(registration_date) as registration_year,
  count(customer_id) as number_of_customers
  from customers 
  where year(registration_date) > 2017 and year(registration_date) < 2025
  group by year(registration_date)
  order by year(registration_date);
+-----------------------------------------------------
  -- b
+-----------------------------------------------------
  select shipping_city, delivered_orders  
 from ( 
  select shipping_city,
@@ -94,7 +114,9 @@ from (
   ) AS shipping_city
 where delivered_orders > 10
 order by delivered_orders desc;
+-----------------------------------------------------
 -- c 
+-----------------------------------------------------
 select product_id, 
 		product_name, 
         sum(quantity) as total_units_sold
@@ -102,7 +124,9 @@ from products
 join order_items using(product_id)
 group by product_id, product_name
 having sum(quantity) > 50;
+-----------------------------------------------------
 -- d 
+-----------------------------------------------------
 select full_name, 
 total_orders 
 from (
@@ -113,7 +137,9 @@ join orders as o using(employee_id)
 group by full_name) as employee_table
 where total_orders >= 20 
 order by total_orders desc;
+-----------------------------------------------------
 -- e 
+-----------------------------------------------------
 select extract( year from order_date) as year,
 		count(order_id) as total_orders, 
         count(distinct(customer_id)) as ordered_customers
@@ -123,25 +149,32 @@ order by year desc;
 -- =========================================================
 -- SECTION D: LIKE AND ILIKE
 -- a 
+-----------------------------------------------------
 select first_name,
 		last_name, 
         email
 from customers
 where email  like '%@gmail.com';
+-----------------------------------------------------
 -- b 
+-----------------------------------------------------
 SELECT product_name,
        category_id,
        unit_price
 FROM products
 WHERE product_name LIKE '%set%'
 ORDER BY unit_price DESC;
--- c 
+-----------------------------------------------------
+-- c
+-----------------------------------------------------
 SELECT CONCAT(first_name,' ',last_name) AS full_name,
        city,
        registration_date
 FROM customers
 WHERE last_name LIKE 'ad%';
+-----------------------------------------------------
 -- d 
+-----------------------------------------------------
 SELECT product_name,
        category_id,
        unit_price
@@ -149,7 +182,9 @@ FROM products
 WHERE product_name LIKE '%combo%'
    OR product_name LIKE '%kit%'
    OR product_name LIKE '%pack%';
+-----------------------------------------------------
    -- e 
+-----------------------------------------------------
    SELECT first_name,
        last_name,
        city
@@ -161,6 +196,7 @@ ORDER BY city,
 -- =========================================================
 -- SECTION E: JOINS
 -- a 
+-----------------------------------------------------
 select order_id, 
 		concat(c.first_name, ' ', c.last_name) as customer_name, 
 		concat(e.first_name, ' ', e.last_name) as employee_name,
@@ -172,7 +208,9 @@ inner join customers as c using(customer_id)
 inner join employees as e
 using(employee_id)
 order by order_date desc limit 50;  
+-----------------------------------------------------
 -- b 
+-----------------------------------------------------
 select customer_id, 
 		concat(first_name, ' ', last_name) as full_name, 
 		city, 
@@ -182,7 +220,9 @@ left join orders
 using(customer_id)
 group by customer_id
 Order by order_count desc, last_name;
+-----------------------------------------------------
 -- c 
+-----------------------------------------------------
 select order_id, 
 		order_date, 
         concat(first_name, ' ', last_name) as customer_full_name, 
@@ -199,7 +239,9 @@ using(customer_id)
 join products
 using(product_id);
 -- group by product_name;
+-----------------------------------------------------
 -- d 
+-----------------------------------------------------
 select  concat(first_name, ' ', last_name) as full_name, 
 		role, 	
         region_name,
@@ -208,7 +250,9 @@ from employees
 join regions using(region_id)
 left join orders using(employee_id)
 group by first_name, last_name, role, region_name;
+-----------------------------------------------------
 -- e 
+-----------------------------------------------------
 select category_name, 
 		product_name, 
         count(distinct(order_id)) as times_ordered, 
@@ -221,6 +265,7 @@ order by total_qty_sold;
 -- =========================================================
 -- SECTION F: Cases
 -- a 
+-----------------------------------------------------
 select product_name, 
 category_name, unit_price,
 case 
@@ -232,7 +277,9 @@ from products
 join categories 
 using(category_id)
 order by unit_price desc;
+-----------------------------------------------------
 -- b 
+-----------------------------------------------------
 select concat(first_name, ' ', last_name) as full_name, role, salary,
 case
 when salary >= 100000 then 'Executive' 
@@ -241,7 +288,9 @@ when salary < 80000 then 'Entry Level'
 end as pay_band
 from employees
 order by salary desc;
+-----------------------------------------------------
 -- c 
+-----------------------------------------------------
 select o.order_id,
 		o.order_date, 
         o.status, 
@@ -250,7 +299,9 @@ from orders as o
 join order_items as oi 
 on o.order_id = oi.order_id
 group by o.order_id, o.order_date, o.status, total_order_value;
+-----------------------------------------------------
 -- d 
+-----------------------------------------------------
 SELECT
     c.category_name,
 
@@ -284,26 +335,33 @@ ORDER BY c.category_name;
 -- =========================================================
 -- SECTION G: SUBQUERIES
 -- a
+-----------------------------------------------------
 select  product_name, category_id, unit_price
 from products 
 where unit_price > (
 select avg(unit_price) 
 from products)
 order by unit_price;
+-----------------------------------------------------
 -- b 
+-----------------------------------------------------
 select concat(first_name, ' ', last_name) as full_name, city
 from customers
 where customer_id in(
 select customer_id
 from orders);
+-----------------------------------------------------
 -- c 
+-----------------------------------------------------
 select product_id, product_name, 
 category_id,  unit_price
 from products
 where product_id not in(
 select product_id
 from order_items);
+-----------------------------------------------------
 -- d 
+-----------------------------------------------------
 SELECT
     customer_name,
     city,
@@ -335,7 +393,9 @@ FROM
 
 ORDER BY total_revenue DESC
 LIMIT 5;
+-----------------------------------------------------
 -- e 
+-----------------------------------------------------
 SELECT
     customer_name,
     city,
@@ -387,6 +447,7 @@ ORDER BY total_revenue DESC;
 -- =========================================================
 -- SECTION H: CTEs
 -- a
+-----------------------------------------------------
 WITH customer_revenue AS (
     SELECT c.customer_id,
            CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
@@ -408,7 +469,9 @@ SELECT *
 FROM customer_revenue
 ORDER BY total_revenue DESC
 LIMIT 10;
+-----------------------------------------------------
  -- b 
+-----------------------------------------------------
  WITH product_sales AS
 (
     SELECT
@@ -433,7 +496,9 @@ WHERE total_qty_sold =
     FROM product_sales ps2
     WHERE ps.category_name = ps2.category_name
 );
+-----------------------------------------------------
 -- c 
+-----------------------------------------------------
 WITH monthly_revenue AS
 (
     SELECT
@@ -474,7 +539,9 @@ SELECT
 FROM monthly_revenue
 CROSS JOIN avg_revenue
 ORDER BY month_no;
+-----------------------------------------------------
 -- d 
+-----------------------------------------------------
 WITH customer_orders AS
 (
     SELECT
@@ -507,7 +574,9 @@ FROM customer_orders
 GROUP BY segment
 
 ORDER BY customer_count DESC;
+-----------------------------------------------------
 -- e 
+-----------------------------------------------------
 WITH yearly_revenue AS
 (
     SELECT
